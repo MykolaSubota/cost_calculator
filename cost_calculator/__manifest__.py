b19662c0609d56712b9bdae93aae1b34a2126ad0
MykolaSubotaKochut <sys.adm.kochut@gmail.com>
2023-08-18 11:44:04 +0300

    fix translate

--- cost_calculator/__manifest__.py
+++ cost_calculator/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Price Calculator',
     'author': 'Mykola Subota',
-    'version': '0.1.0.1',
+    'version': '0.1.0.2',
     'summary': 'Calculator for calculating the cost of tables.',
     'sequence': 100,
     'category': 'Inventory/Inventory',
