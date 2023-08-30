039a274aa505fee815c06699487187433bc6c359
MykolaSubotaKochut <sys.adm.kochut@gmail.com>
2023-08-30 15:25:54 +0300

    fix

--- cost_calculator/__manifest__.py
+++ cost_calculator/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Price Calculator',
     'author': 'Mykola Subota',
-    'version': '0.1.0.3',
+    'version': '0.1.0.4',
     'summary': 'Calculator for calculating the cost of tables.',
     'sequence': 100,
     'category': 'Inventory/Inventory',
---
+++ cost_calculator/models/__init__.py
@@ -0,0 +1,1 @@
+from . import component_production, record_calculator, parameters_calculator
